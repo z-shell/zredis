bc05c108e43c5fe900a564dd3853b38f6dd2a0e2
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-05-24 08:46:03 +0200

    Update license header

--- module/Src/zdharma/zredis.c
+++ module/Src/zdharma/zredis.c
@@ -1,12 +1,10 @@
 /*
- * zgdbm.c - bindings for gdbm
+ * zredis.c - bindings for (hi)redis
  *
- * This file is part of zsh, the Z shell.
- *
- * Copyright (c) 2008 Clint Adams
+ * Based on db_gdbm.c from Zsh distribution, Copyright (c) 2008 Clint Adams
  * All rights reserved.
  *
- * Modifications copyright (c) 2017 Sebastian Gniazdowski
+ * Copyright (c) 2017 Sebastian Gniazdowski
  * All rights reserved.
  *
  * Permission is hereby granted, without written agreement and without
@@ -14,21 +12,6 @@
  * software and to distribute modified versions of this software for any
  * purpose, provided that the above copyright notice and the following
  * two paragraphs appear in all copies of this software.
- *
- * In no event shall Clint Adams or the Zsh Development
- * Group be liable to any party for direct, indirect, special, incidental, or
- * consequential damages arising out of the use of this software and its
- * documentation, even if Peter Stephenson, Sven Wischnowsky and the Zsh
- * Development Group have been advised of the possibility of such damage.
- *
- * Clint Adams and the Zsh Development Group
- * specifically disclaim any warranties, including, but not limited to, the
- * implied warranties of merchantability and fitness for a particular purpose.
- * The software provided hereunder is on an "as is" basis, and Peter
- * Stephenson, Sven Wischnowsky and the Zsh Development Group have no
- * obligation to provide maintenance, support, updates, enhancements, or
- * modifications.
- *
  */
 
 #include "zgdbm.mdh"
