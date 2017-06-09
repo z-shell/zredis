b9d29abaca2c49734f2e76c4200730cfe6b644a4
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-06-09 07:32:06 +0200

    test: Better Valgrind test-type switching mechanism

--- module/ValTest/runtests.zsh
+++ module/ValTest/runtests.zsh
@@ -7,18 +7,18 @@ emulate zsh
 # protect from catastrophic failure of an individual test.
 # We could probably do that with subshells instead.
 
+[[ -f "test_type" ]] && local tpe=$(<test_type)
+local cmd="valgrind"
+(( ${+commands[colour-valgrind]} )) && cmd="colour-valgrind"
+
 integer success failure skipped retval
 for file in "${(f)ZTST_testlist}"; do
-  if (( ${+commands[colour-valgrind]} )); then
-      if [[ 3 -nt 2 && 3 -nt 1 ]]; then
-          colour-valgrind --leak-check=full --show-possibly-lost=no $ZTST_exe +Z -f $ZTST_srcdir/ztst.zsh $file
-      elif [[ 2 -nt 1 ]]; then
-          colour-valgrind $ZTST_exe +Z -f $ZTST_srcdir/ztst.zsh $file
-      else
-          colour-valgrind --leak-check=full $ZTST_exe +Z -f $ZTST_srcdir/ztst.zsh $file
-      fi
+  if [[ "$tpe" = "3" ]]; then
+    $cmd --leak-check=full --show-possibly-lost=no $ZTST_exe +Z -f $ZTST_srcdir/ztst.zsh $file
+  elif [[ "$tpe" = "2" ]]; then
+    $cmd $ZTST_exe +Z -f $ZTST_srcdir/ztst.zsh $file
   else
-      valgrind --leak-check=full $ZTST_exe +Z -f $ZTST_srcdir/ztst.zsh $file
+    $cmd --leak-check=full $ZTST_exe +Z -f $ZTST_srcdir/ztst.zsh $file
   fi
   retval=$?
   if (( $retval == 2 )); then
