75cccf04948c4913ee4e68bdb40214af3b312fec
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-06-16 12:57:47 +0200

    val: Copyright notice

--- module/ValTest/zsh-valgrind-parse.cmd
+++ module/ValTest/zsh-valgrind-parse.cmd
@@ -1,6 +1,9 @@
 #!/bin/sh
 # -*- Mode: sh; sh-indentation: 4; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # vim:ft=zsh:sw=4:sts=4:et
+#
+# Copyright (c) 2017 Sebastian Gniazdowski
+# All rights reserved.
 
 #
 # Setup
